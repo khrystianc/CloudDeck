9ca0a8105c4b8f7b73b39da2137da35ef9e89981
Khrystian Clark <khrystian.clark@gmail.com>
2025-04-21 12:56:48 -0700

    Create templates.py

---
+++ backend/app/routes/templates.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from app.utils.templates import get_available_templates
+
+bp = Blueprint('templates', __name__, url_prefix='/api/templates')
+
+@bp.route('/', methods=['GET'])
+def list_templates():
+  templates = get_available_templates()
+  return jsonify({"templates": templates})
