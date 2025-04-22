9b8b8fca7a7b4119b2db7e31632c129fe8869970
Khrystian Clark <khrystian.clark@gmail.com>
2025-04-22 07:45:16 -0700

    Update github.py

--- backend/app/routes/github.py
+++ backend/app/routes/github.py
@@ -1,4 +1,4 @@
 from flask import Blueprint
 
-bp = Blueprint('github', __name__. url_prefix='/api.github')
+bp = Blueprint('github', __name__. url_prefix='/api/github')
 # Placeholder define the github integration route logic here
