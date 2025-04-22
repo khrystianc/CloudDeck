7c5e715ae031c410e3138462a7fcea66ce1c8b2a
Khrystian Clark <khrystian.clark@gmail.com>
2025-04-22 08:16:50 -0700

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import StackLauncher from './components/StackLauncher';
+
+function App() {
+  return (
+    <div classname="App">
+      <h1>CloudDeck Launcher</h1>
+      <StackLauncher />
+    </div>
+  );
+}
+
+export default App;
