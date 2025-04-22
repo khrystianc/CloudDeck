c1e05c9c522d66845a5802fdfdfa8e2742a1f24d
Khrystian Clark <khrystian.clark@gmail.com>
2025-04-22 12:54:31 -0700

    Update templates.py

--- backend/app/routes/templates.py
+++ backend/app/routes/templates.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, Blueprint
 from app.utils.templates import get_available_templates
 
 bp = Blueprint('templates', __name__, url_prefix='/api/templates')
